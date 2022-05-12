ff91677207820c65f9515d261f57d4dc9cef8166
tmf566@gmail.com <tmf566@gmail.com>
2022-05-12 17:09:57 +0900

    更新

---
+++ gitlab/gitlab_runner.bat
@@ -0,0 +1,19 @@
+
+echo %date%
+echo %time%
+
+SET yyyy=%date:~0,4%
+SET mm=%date:~5,2%
+SET dd=%date:~8,2%
+
+SET time2=%time: =0%
+
+SET hh=%time2:~0,2%
+SET mn=%time2:~3,2%
+SET ss=%time2:~6,2%
+
+SET filename=%yyyy%-%mm%%dd%-%hh%%mn%%ss%
+SET LOG=C:\work\log\k8_gitlab_runner_%filename%.txt
+cd C:\work\kubernetes\gitlab>
+
+kubectl apply -f runner.yaml  >> %LOG%
